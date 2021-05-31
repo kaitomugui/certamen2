2dcb3de4d94b9d13278a17d3d2b32b40daadab55
Franco Ortega <caco.ortega@hotmail.com>
2021-05-31 16:48:31 -0400

    Comprovacion pass y se agrego nueva actividad menu

--- app/src/main/java/com/example/certamen2/MainActivity.kt
+++ app/src/main/java/com/example/certamen2/MainActivity.kt
@@ -2,10 +2,39 @@ package com.example.certamen2
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val usuario = "uvm"
+        val password = "pwm2021"
+
+        val etUsuario = findViewById<EditText>(R.id.etUsuario)
+        val etContra = findViewById<EditText>(R.id.etContra)
+
+        val btLogin = findViewById<Button>(R.id.btLogin)
+        val btReset = findViewById<Button>(R.id.btReset)
+
+        btLogin.setOnClickListener {
+            if(etContra.text.toString().length == 0 || etUsuario.text.toString().length == 0)
+                Toast.makeText(this,"Los campos no pueden quedar vacíos", Toast.LENGTH_LONG).show()
+
+            if(etUsuario.text.toString() == usuario && etContra.text.toString() == password) {
+                Toast.makeText(this, "Conexion Correcta", Toast.LENGTH_LONG).show()
+
+            }
+        }
+
+        btReset.setOnClickListener {
+            etUsuario.setText("")
+            etContra.setText("")
+
+        }
+
     }
 }
