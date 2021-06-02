2bd90ffd84051edbf90a975d79eeb409f9b7da9e
Tyra-hub <80894394+Tyra-hub@users.noreply.github.com>
2021-06-01 20:31:25 -0400

    Se agrega el acerca de

---
+++ app/src/main/java/com/example/certamen2/AcercaDe.kt
@@ -0,0 +1,13 @@
+package com.example.certamen2
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class AcercaDe : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_acerca_de)
+
+        val
+    }
+}
