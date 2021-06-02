ff45d3fb002199a74181e23453d3e52353389256
Tyra-hub <80894394+Tyra-hub@users.noreply.github.com>
2021-06-01 20:47:55 -0400

    Se agregan los datos respectivos al acerca de

--- app/src/main/java/com/example/certamen2/AcercaDe.kt
+++ app/src/main/java/com/example/certamen2/AcercaDe.kt
@@ -2,12 +2,36 @@ package com.example.certamen2
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
 
 class AcercaDe : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_acerca_de)
 
-        val
+        val TextViewADV = findViewById<EditText>(R.id.TextViewADV)
+        val TextViewADD = findViewById<EditText>(R.id.TextViewADD)
+        val TextViewADC = findViewById<EditText>(R.id.TextViewADC)
+        val ButtonASalir = findViewById<Button>(R.id.ButtonASalir)
+
+
+        TextViewADV.text "Version 1.0.0"
+        TextViewADD.text "Esta aplicacion es una base de datos con el cual puede ingresar nuevos datos modificarlos y eliminarlos sin generar conflictos entre un auto y otro"
+        TextViewADC.text "Desarrolladores: Franco Ortega Humberto Ponce Joaquin Hernandez"
+
+
+
+
+
+
+
+
+
+
+
+        ButtonASalir.setOnClickListener {
+            finish()
+        }
     }
 }
